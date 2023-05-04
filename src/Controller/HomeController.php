1103bd994525f86a771341547841583862c6c506
ahmed <ahmed-1611@live.fr>
2023-05-04 21:51:01 +0200

    first commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,46 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+
+        $questions =[
+            [
+                'title' => 'je suis une super question',
+                'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque reprehenderit ratione eveniet iste quidem itaque deleniti esse corrupti, ipsum recusandae at omnis quibusdam vero incidunt officiis odio, excepturi tempore natus.',
+                'rating' => 20,
+                'author' => [
+                    'name'=>'Jean Dupont',
+                    'avatar' => 'https://randomuser.me/api/portraits/men/1.jpg'
+                ],
+                'nbrOfResponse' => 15
+
+            ],
+            [
+                'title' => 'je suis une super question',
+                'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque reprehenderit ratione eveniet iste quidem itaque deleniti esse corrupti, ipsum recusandae at omnis quibusdam vero incidunt officiis odio, excepturi tempore natus.',
+                'rating' => 0,
+                'author' => [
+                    'name'=>'Alex Miro',
+                    'avatar' => 'https://randomuser.me/api/portraits/men/30.jpg'
+                ],
+                'nbrOfResponse' => 20
+
+            ],
+            [
+                'title' => 'je suis une super question',
+                'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque reprehenderit ratione eveniet iste quidem itaque deleniti esse corrupti, ipsum recusandae at omnis quibusdam vero incidunt officiis odio, excepturi tempore natus.',
+                'rating' => -15,
+                'author' => [
+                    'name'=>'Dan Delarue',
+                    'avatar' => 'https://randomuser.me/api/portraits/men/10.jpg'
+                ],
+                'nbrOfResponse' => 30
+
+            ],
+
+        ];
+   
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'questions' => $questions
         ]);
     }
 }
